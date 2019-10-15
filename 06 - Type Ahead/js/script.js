c9b4ed2d7d361532751e7bd1bde58b5e961b268f
Cherryboome <muhamidifarian@yahoo.com>
2019-10-15 14:41:16 +0300

    completed Ajax Type Ahead

---
+++ 06 - Type Ahead/js/script.js
@@ -0,0 +1,51 @@
+const endpoint =
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+const cities = [];
+
+fetch(endpoint)
+  .then(rawData => rawData.json())
+  .then(data => cities.push(...data));
+
+const findMatches = (wordToWatch, cities) => {
+  return cities.filter(place => {
+    const regex = new RegExp(wordToWatch, "gi");
+    return place.city.match(regex) || place.state.match(regex);
+  });
+};
+
+// const numberWithCommas = x => {
+//   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+// };
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  const html = matchArray
+    .map(place => {
+      const regex = new RegExp(this.value, "gi");
+      const cityName = place.city.replace(
+        regex,
+        `<span class="hl">${this.value}</span>`
+      );
+      const stateName = place.state.replace(
+        regex,
+        `<span class="hl">${this.value}</span>`
+      );
+      return `
+      <li>
+        <span>${cityName}, ${stateName}</span>
+        <span class="population">${Number(
+          place.population
+        ).toLocaleString()}</span>
+      </li>
+      `;
+    })
+    .join("");
+  suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
+
+searchInput.addEventListener("change", displayMatches);
+searchInput.addEventListener("keyup", displayMatches);
