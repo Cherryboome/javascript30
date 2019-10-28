f230d891f88e56ed795df1d7200594354f0f818c
Cherryboome <muhamidifarian@yahoo.com>
2019-10-28 11:29:31 +0200

    corrected yWalk formula

--- 16 - Mouse Move Shadow/js/scripts.js
+++ 16 - Mouse Move Shadow/js/scripts.js
@@ -7,12 +7,12 @@ function shadow(e) {
   let { offsetX: x, offsetY: y } = e;
 
   if (this !== e.target) {
-    x = x + e.target.offsetLeft;
-    y = y + e.target.offsetTop;
+    x += e.target.offsetLeft;
+    y += e.target.offsetTop;
   }
 
   const xWalk = Math.round((x / width) * walk - walk / 2);
-  const yWalk = Math.round((y / width) * walk - walk / 2);
+  const yWalk = Math.round((y / height) * walk - walk / 2);
 
   text.style.textShadow = `
   ${xWalk}px ${yWalk}px 0 rgba(255, 0, 255, 0.7),
